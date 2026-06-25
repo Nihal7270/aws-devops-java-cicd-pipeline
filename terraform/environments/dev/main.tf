3062e9a4af4ba572a9bd93ac8bdfac12f722d990
Nihal Allugula <allugulan@gmail.com>
2026-06-25 19:05:09 +0530

    Update main.tf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -78,15 +78,15 @@ module "artifact_s3" {
   bucket_name  = var.artifact_bucket_name
   project_name = var.project_name
 }
-# module "alb" {
-#   source = "../../modules/alb"
-#
-#   project_name        = var.project_name
-#   vpc_id              = module.vpc.vpc_id
-#   subnet_ids          = [module.vpc.public_subnet_1_id, module.vpc.public_subnet_2_id]
-#   alb_sg_id           = module.security_group.alb_sg_id
-#   target_instance_ids = [
-#     module.tomcat_server_1.instance_id,
-#     module.tomcat_server_2.instance_id
-#   ]
-# }
+ module "alb" {
+   source = "../../modules/alb"
+
+   project_name        = var.project_name
+   vpc_id              = module.vpc.vpc_id
+   subnet_ids          = [module.vpc.public_subnet_1_id, module.vpc.public_subnet_2_id]
+   alb_sg_id           = module.security_group.alb_sg_id
+   target_instance_ids = [
+     module.tomcat_server_1.instance_id,
+     module.tomcat_server_2.instance_id
+   ]
+ }
