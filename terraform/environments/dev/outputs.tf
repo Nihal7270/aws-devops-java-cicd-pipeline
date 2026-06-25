8403c8a7395cf52d08ef33a7f0a935c038c84bc6
Nihal Allugula <allugulan@gmail.com>
2026-06-25 19:05:49 +0530

    Restore ALB output variables in outputs.tf

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -66,14 +66,14 @@ output "artifact_bucket_arn" {
   value = module.artifact_s3.bucket_arn
 }
 
-#output "alb_dns_name" {
-#  value = module.alb.alb_dns_name
-#}
-#
-#output "alb_arn" {
-#  value = module.alb.alb_arn
-#}
-#
-#output "target_group_arn" {
-#  value = module.alb.target_group_arn
-#}
+output "alb_dns_name" {
+  value = module.alb.alb_dns_name
+}
+
+output "alb_arn" {
+  value = module.alb.alb_arn
+}
+
+output "target_group_arn" {
+  value = module.alb.target_group_arn
+}
